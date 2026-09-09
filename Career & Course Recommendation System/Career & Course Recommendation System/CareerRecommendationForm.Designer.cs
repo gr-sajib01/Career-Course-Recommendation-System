40e6ebe73eb0666a3d11ed069f88be47b1f415f5
Md Golam Rabbani Sajib <grsajib600@gmail.com>
2026-09-10 00:36:48 +0600

    Create CareerRecommendationForm.Designer.cs

---
+++ Career & Course Recommendation System/Career & Course Recommendation System/CareerRecommendationForm.Designer.cs
@@ -0,0 +1,10 @@
+﻿namespace Career___Course_Recommendation_System
+{
+    partial class CareerRecommendationForm
+    {
+        private void InitializeComponent()
+        {
+            // The form UI is created in CareerRecommendationForm.cs
+        }
+    }
+}
